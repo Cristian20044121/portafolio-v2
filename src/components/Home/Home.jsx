5a00c6e43496f02952af06acedd3b608acce732d
Cristian <crdcaro01@gmail.com>
2024-04-03 21:38:38 -0500

    change styles and add information

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  return <div>
+    <div>
+      <div>
+        <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-ninja-e-sports-logo-vector-illustration-ninja-gaming-mascot-png-image_5045023.jpg" />
+      </div>
+      <div>
+        <h1>FULL STACK ENGINNER</h1>
+        <p>
+        I am a web development student with a passion for creating digital solutions. I have a strong command of various technologies and tools that enable me to design and develop innovative web applications. My skillset includes proficiency in as HTML, CSS, Bootstrap,JavaScript, Node Js, Express, Git and Github.I am a web development student with a passion for creating digital solutions. I have a strong command of various technologies and tools that enable me to design and develop innovative web applications. My skillset includes proficiency in as HTML, CSS, Bootstrap,JavaScript, Node Js, Express, Git and Github.
+        </p>
+      </div>
+    </div>
+  </div>
 };
 
 export default Home;
