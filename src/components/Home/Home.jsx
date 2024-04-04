6cdb7c9bc874d71cf4230b7963dfdaae3bed2176
crdcaro01@gmail.com <crdcaro01@gmail.com>
2024-04-04 13:22:42 -0500

    add and modified code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,72 @@
 import React from "react";
-
+import photo from "../../img/icon-profile.png";
+import "./style.css";
 const Home = () => {
-  return <div>
-    <div>
-      <div>
-        <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-ninja-e-sports-logo-vector-illustration-ninja-gaming-mascot-png-image_5045023.jpg" />
-      </div>
-      <div>
-        <h1>FULL STACK ENGINNER</h1>
-        <p>
-        I am a web development student with a passion for creating digital solutions. I have a strong command of various technologies and tools that enable me to design and develop innovative web applications. My skillset includes proficiency in as HTML, CSS, Bootstrap,JavaScript, Node Js, Express, Git and Github.I am a web development student with a passion for creating digital solutions. I have a strong command of various technologies and tools that enable me to design and develop innovative web applications. My skillset includes proficiency in as HTML, CSS, Bootstrap,JavaScript, Node Js, Express, Git and Github.
-        </p>
+  return (
+    <div className="container-home">
+      <div className="flex items-center justify-around w-11/12 mx-auto mt-28">
+        <div className="w-3/4">
+          <h1 className="title-home text-8xl font-semibold text-cyan-500">
+            FULL STACK <span class="title-home-span">ENGINEER</span>
+          </h1>
+          <p className="home-text text-lg mt-10">
+            I am a web development student with a passion for creating digital
+            solutions. I have a strong command of various technologies and tools
+            that enable me to design and develop innovative web applications. My
+            skillset includes proficiency in as HTML, CSS, Bootstrap,JavaScript,
+            Node Js, Express, Git and Github.I am a web development student with
+            a passion for creating digital solutions. I have a strong command of
+            various technologies and tools that enable me to design and develop
+            innovative web applications. My skillset includes proficiency in as
+            HTML, CSS, Bootstrap,JavaScript, Node Js, Express, Git and Github.
+          </p>
+          <br />
+
+          <div className="home-sci w-1/4">
+            <a
+              href="https://github.com/Cristian20044121"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl"
+            >
+              <i className="bx bxl-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/cristian-caro-9b4040260/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl"
+            >
+              <i className="bx bxl-linkedin"></i>
+            </a>
+            <a
+              href="https://api.whatsapp.com/send?phone=573005208105"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl"
+            >
+              <i className="bx bxl-whatsapp"></i>
+            </a>
+            <a
+              href="https://drive.google.com/file/d/1TMWiswTmNWuj0ckyACWyWh-UbZzgVwKs/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl"
+            >
+              CV
+            </a>
+          </div>
+        </div>
+        <div className="w-4/5 text-center">
+          <img
+            src={photo}
+            alt="profile"
+            className="mx-auto image-profile cursor-pointer  transition duration-300 ease-in-out"
+          />
+        </div>
       </div>
     </div>
-  </div>
+  );
 };
 
 export default Home;
