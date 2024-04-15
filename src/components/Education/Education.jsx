fde6993298b3af80faae60b010f1109e87f63bc2
crdcaro01@gmail.com <crdcaro01@gmail.com>
2024-04-15 13:38:28 -0500

    change of component education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import "./style.css";
 
 const Education = () => {
-  return <div>eduaction</div>;
+  return (
+    <div className="mx-auto mt-28 border border-red-500 w-2/3 text-center">
+      <h1>EDUCATION</h1>
+      <p></p>
+      <div className="border border-blue-500 grid grid-rows-4 grid-flow-col flex-wrap">
+        <div class="parent">
+          <div class="div1">
+            <h2 className="text-2xl font-bold italic">2022</h2> <br />
+            <p className="font-bold">DISEÑO WEB CON ADOBE DREAMWEAVER</p>
+          </div>
+          <div class="div2">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2022</h2> <br />
+            <p className="font-bold">CURSO PROFESIONAL DE JAVASCRIPT</p>
+          </div>
+          <div class="div3">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">
+              VARIABLES Y ESTRUCTURAS DE CONTROL EN LA PROGRAMACION ORIENTADA A
+              OBJETOS: JAVA
+            </p>{" "}
+          </div>
+          <div class="div4">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">
+              ANALISIS PARA EL DESARROLLO MOVIL CON APP INVENTOR
+            </p>
+          </div>
+          <div class="div5">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2022-2023</h2> <br />
+            <p className="font-bold">
+              Educación IT - Bootcamp Full Stack Engineer
+            </p>
+          </div>
+          <div class="div6">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">
+              CONCEPTOS DE PYTHON PARA BACKEND - BOOTCAMP BACKEND CON PYTHON
+            </p>
+          </div>
+          <div class="div7">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">IMPLEMENTACION DE BASES DE DATOS NOSQL</p>
+          </div>
+          <div class="div8">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023-2024</h2> <br />
+            <p className="font-bold">
+              TÉCNICO EN PROGRAMACIÓN DE APLICACIONES Y SERVICIOS PARA LA NUBE
+            </p>
+          </div>
+          <div class="div9">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">
+              CONSTRUCCION DE BASES DE DATOS CON MYSQL
+            </p>
+          </div>
+          <div class="div10">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">
+              APLICACION DEL MARCO DE TRABAJO SCRUM PARA PROYECTOS DE DESARROLLO
+              DE SOFTWARE
+            </p>{" "}
+          </div>
+          <div class="div11">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">CURSO CSS</p>{" "}
+          </div>
+          <div class="div12">
+            {" "}
+            <h2 className="text-2xl font-bold italic">2023</h2> <br />
+            <p className="font-bold">Introducción a UX</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
